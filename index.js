82eaae036ced78747f61c769e532399443a4f943
Ashok <dhunganaashok312@gmail.com>
2023-04-28 23:56:19 +0545

    testing git

---
+++ index.js
@@ -0,0 +1,13 @@
+class Railwayform{
+    submit(){
+        alert("form submitted")
+    }
+    cancel(){
+        alert("This form is cancelled")
+    }
+}
+let Laxmi = new Railwayform()
+let Ashok = new Railwayform()
+Laxmi.submit()
+Ashok.submit()
+Laxmi.cancel()
